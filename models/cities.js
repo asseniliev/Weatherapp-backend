3cd861e2e6530530f04f235f527e5afb9c540e16
asseniliev <assen.s.iliev@gmail.com>
2023-03-31 22:57:52 +0200

    modify the weather api

--- models/cities.js
+++ models/cities.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
+// const citySchema = mongoose.Schema({
+// 	cityName: String,
+// 	main: String,
+// 	description: String,
+// 	tempMin: Number,
+// 	tempMax: Number,
+// });
+
 const citySchema = mongoose.Schema({
 	cityName: String,
-	main: String,
-	description: String,
-	tempMin: Number,
-	tempMax: Number,
 });
 
 const City = mongoose.model('cities', citySchema);
--- routes/weather.js
+++ routes/weather.js
@@ -5,54 +5,57 @@ const City = require('../models/cities');
 
 const OWM_API_KEY = process.env.OWM_API_KEY;
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+
 	// Check if the city has not already been added
-	City.findOne({ cityName: { $regex: new RegExp(req.body.cityName, 'i') } }).then(dbData => {
-		if (dbData === null) {
-			// Request OpenWeatherMap API for weather data
-			fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityName}&appid=${OWM_API_KEY}&units=metric`)
-				.then(response => response.json())
-				.then(apiData => {
-					//Make the city's first letter always be capital
-					let cityName = req.body.cityName;
-					cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
-					// Creates new document with weather data
-					const newCity = new City({
-						cityName: cityName,
-						main: apiData.weather[0].main,
-						description: apiData.weather[0].description,
-						tempMin: apiData.main.temp_min,
-						tempMax: apiData.main.temp_max,
-					});
+	const foundCity = await City.findOne({ cityName: { $regex: new RegExp(req.body.cityName, 'i') } });
 
-					// Finally save in database
-					newCity.save().then(newDoc => {
-						res.json({ result: true, weather: newDoc });
-					});
-				});
-		} else {
-			// City already exists in database
-			res.json({ result: false, error: 'City already saved' });
-		}
-	});
+	//If the city exists - we don't do anything
+	if (foundCity !== null) {
+		res.json({ result: false, error: 'City already saved' });
+		return;
+	}
+
+	const apiData = await getCityWeather(req.body.cityName);
+
+	//if city is found in the weather api data
+	if (apiData.result) {
+
+		const newCity = new City({
+			cityName: apiData.cityWeather.cityName
+		});
+		await newCity.save();
+	}
+
+	res.json(apiData);
 });
 
-router.get('/', (req, res) => {
-	City.find().then(data => {
-		res.json({ weather: data });
-	});
+router.get('/', async (req, res) => {
+	const cities = await City.find();
+
+	const weatherData = [];
+	for (const city of cities) {
+		const cityWeather = await getCityWeather(city.cityName);
+		weatherData.push(cityWeather);
+	}
+	res.json({ weather: weatherData });
 });
 
-router.get("/:cityName", (req, res) => {
-	City.findOne({
+router.get("/:cityName", async (req, res) => {
+
+	const cityFound = await City.findOne({
 		cityName: { $regex: new RegExp(req.params.cityName, "i") },
-	}).then(data => {
-		if (data) {
-			res.json({ result: true, weather: data });
-		} else {
-			res.json({ result: false, error: "City not found" });
-		}
 	});
+
+	if (cityFound === null) {
+		res.json({ result: false, error: "City not found" });
+		return;
+	}
+
+	const weatherData = await getCityWeather(req.params.cityName);
+
+	res.json({ weatherData });
+
 });
 
 router.delete("/:cityName", (req, res) => {
@@ -69,5 +72,26 @@ router.delete("/:cityName", (req, res) => {
 		}
 	});
 });
+
+
+async function getCityWeather(cityName) {
+
+	const apiData = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${OWM_API_KEY}&units=metric`)).json();
+
+	if (apiData.cod === "404") {
+		return { result: false, error: 'City does not exist' };
+	}
+
+	cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+	const cityWeather = {
+		cityName: cityName,
+		main: apiData.weather[0].main,
+		description: apiData.weather[0].description,
+		tempMin: apiData.main.temp_min,
+		tempMax: apiData.main.temp_max,
+	}
+
+	return { result: true, cityWeather: cityWeather };;
+}
 
 module.exports = router;
