b6734b899ceb0cfcb9cab044f5be7dc32f3c1c6d
asseniliev <assen.s.iliev@gmail.com>
2023-03-31 18:21:40 +0200

    add users.js routesh

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send({ result: "AAAA" });
+const { checkBody } = require('../modules/checkBody');
+
+var User = require("../models/users");
+
+router.post("/signup", (req, res) => {
+  console.log("Signup called");
+
+  if (checkBody(req.body, ['email', 'password'])) {
+    User.findOne({ email: req.body.email }).then((data) => {
+      if (data) {
+        res.json({ result: false, error: "User already exists" });
+      } else {
+        console.log('in else');
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        console.log(newUser);
+        newUser.save().then((data) => {
+          res.json({ result: true });
+        });
+      }
+    })
+  } else {
+    //console.log("Email: " + req.body.email);
+    res.json({ result: false, error: "Missing or empty fields" });
+  }
 });
+
+router.post("/signin", (req, res) => {
+  if (checkBody(req.body), ['email', 'password']) {
+    User.findOne({ email: req.body.email, password: req.body.password }).
+      then((data) => {
+        if (data) res.json({ result: true });
+        else res.json({ result: false, error: "User not found" });
+      }
+      );
+  } else {
+    res.json({ result: false, error: "Missing or empty fields" });
+  }
+});
+
 
 module.exports = router;
